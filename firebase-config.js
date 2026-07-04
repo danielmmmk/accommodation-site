22756188daa31c1b4d37f2157ab566700b0e10a8
danielmmmk <dm.mckeogh@hotmail.com>
2026-07-04 19:13:34 +1000

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDSlyCOnqlXUseDOx-0fw-BwGARx9-3kYY",
+  authDomain: "korea-trip-c16ba.firebaseapp.com",
+  projectId: "korea-trip-c16ba",
+  storageBucket: "korea-trip-c16ba.firebasestorage.app",
+  messagingSenderId: "1069612295844",
+  appId: "1:1069612295844:web:b4c8a34df4d8e946472831",
+  measurementId: "G-Z440E6RWJQ"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
