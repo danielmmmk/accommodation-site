7e96ee3d1ea03e90e02e35e5c665fd5c624b9db0
danielmmmk <dm.mckeogh@hotmail.com>
2026-07-04 19:55:53 +1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-import { getAuth } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js';
+<script type="module">
+   import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js';
+   import { getAuth } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
+   import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js';
+</script>
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
