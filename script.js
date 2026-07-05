4f48f59f5056a0ec718269c58e918160939027a8
danielmmmk <dm.mckeogh@hotmail.com>
2026-07-05 10:13:48 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-import { auth, db, doc, setDoc, serverTimestamp } from "./firebase-config.js";
+import { doc, setDoc, serverTimestamp } from 'https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js'
+import { auth, db } from "./firebase-config.js";
 
 window.vote = async function(accommodationId, voteValue) {
 
